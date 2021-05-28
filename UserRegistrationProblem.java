b0518ac96463926f05df84d1bc39fcc0cb92cc64
VaibhavJagtap2 <vaibhavjagtap03.vj@gmail.com>
2021-05-28 23:52:05 +0530

    Welcome to User Registration

---
+++ UserRegistrationProblem.java
@@ -0,0 +1,8 @@
+package com.userregistrationproblem;
+
+
+public class UserRegistrationProblem {
+	public static void main(String[] args) {
+		System.out.println("Welcome To The User Regestration Problems:");
+	}
+}
